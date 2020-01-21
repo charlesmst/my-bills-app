d8f95a23266c0e824c00a30d982be616827d3778
Charles Stein <charles.mst@gmail.com>
2020-01-21 10:30:00 -0300

    add react riyter

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders home page", () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(wrapper.find("h2").text()).toEqual("Home");
 });

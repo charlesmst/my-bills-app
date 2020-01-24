b298fc6b03bfb11456509baebd65341060ab0aef
Charles Stein <charles.mst@gmail.com>
2020-01-24 13:52:24 -0300

    Remove some warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import MainPage from "./components/MainPage";
 import AccountList from "./pages/AccountList";
 import { Provider } from "react-redux";
 import store from "./store";
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 export default function App() {
   return (
     <Provider store={store}>
@@ -28,10 +27,6 @@ export default function App() {
 
 function Home() {
   return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
 }
 
 function Users() {
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 import { fontFamily, fontSize, gray2 } from "../Styles";
 
